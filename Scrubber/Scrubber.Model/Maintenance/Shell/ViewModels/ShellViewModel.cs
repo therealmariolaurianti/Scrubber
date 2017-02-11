2e47cb3c650b443728366c29e13896eb4d70d2af
Mario Laurianti <mlaurianti24@gmail.com>
2017-02-11 13:09:48 -0500

    ui stuff

--- Scrubber/Scrubber.Model/Maintenance/Shell/ViewModels/ShellViewModel.cs
+++ Scrubber/Scrubber.Model/Maintenance/Shell/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using Caliburn.Micro;
 using Scrubber.Factories;
@@ -6,14 +7,24 @@ using Scrubber.Helpers;
 
 namespace Scrubber.Model.Maintenance.Shell.ViewModels
 {
-    public class ShellViewModel : PropertyChangedBase, IShell
+    public class ShellViewModel : Screen, IShell
     {
         private readonly IBathtubFactory _bathtubFactory;
         private string _folderPath;
+        private bool _columnThenRow;
+        private bool _rowThenColumn;
+        private bool _isLoading;
 
         public ShellViewModel(IBathtubFactory bathtubFactory)
         {
             _bathtubFactory = bathtubFactory;
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+            RowThenColumn = true;
+            DisplayName = "Scrubber";
         }
 
         public string FolderPath
@@ -27,7 +38,57 @@ namespace Scrubber.Model.Maintenance.Shell.ViewModels
             }
         }
 
+        public bool RowThenColumn   
+        {
+            get { return _rowThenColumn; }
+            set
+            {
+                if (value == _rowThenColumn) return;
+                _rowThenColumn = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public bool ColumnThenRow
+        {
+            get { return _columnThenRow; }
+            set
+            {
+                if (value == _columnThenRow) return;
+                _columnThenRow = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                if (value == _isLoading) return;
+                _isLoading = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+
         public ICommand ScrubCommand => new DelegateCommand(RunScrubber);
+
+        public void ColumnThenRowChecked()
+        {
+            if (RowThenColumn)
+                RowThenColumn = false;
+
+            ColumnThenRow = true;
+        }
+
+        public void RowThenColumnChecked()
+        {
+            if (ColumnThenRow)
+                ColumnThenRow = false;
+
+            RowThenColumn = true;
+        }
 
         public void RunScrubber()
         {
@@ -37,13 +98,18 @@ namespace Scrubber.Model.Maintenance.Shell.ViewModels
                 return;
             }
 
-            var bathtub = _bathtubFactory.Create(FolderPath);
+            IsLoading = true;
+            Task.Run(() =>
+            {
+                var bathtub = _bathtubFactory.Create(FolderPath);
 
-            bathtub.Fill();
-            bathtub.Rinse();
+                bathtub.Fill();
+                bathtub.Rinse();
 
-            var result = bathtub.Drain();
-            result.DisplayResult();
+                var result = bathtub.Drain();
+                result.DisplayResult();
+            }).GetAwaiter()
+              .OnCompleted(() => { IsLoading = false; });
         }
     }
 }
