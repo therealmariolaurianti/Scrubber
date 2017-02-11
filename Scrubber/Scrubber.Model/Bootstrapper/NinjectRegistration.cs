e2f39315f5f794788aabcffe631165ceaab1e011
Mario Laurianti <mlaurianti24@gmail.com>
2017-02-11 11:01:35 -0500

    add ninject reg file

---
+++ Scrubber/Scrubber.Model/Bootstrapper/NinjectRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Bootstrap.Ninject;
+using Ninject;
+
+namespace Scrubber.Model.Bootstrapper
+{
+    public class NinjectRegistration : INinjectRegistration
+    {
+        public void Register(IKernel container)
+        {
+            
+        }
+    }
+}
